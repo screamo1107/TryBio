5cea9341efc9a7fab1f3b20626fedd6ea66e1596
screamo1107 <screamo1107@gmail.com>
2020-06-11 18:11:04 +0200

    Initial with the notes

---
+++ notes.py
@@ -0,0 +1,10 @@
+#############
+# TODO list:
+##########################################################################################################
+# 1. Main logic (biopython, flask, requests) - parse sequence / rEndonuclease -> return separated sequence
+# 1.5 Unit tests (unittests / pytest)
+# 2. Store rEndonuclease list within DB (MySQL)
+# 3. FE part (HTML/CSS, Django, update logic to API-based, return rEndonuclease list to FE from DB)
+# 3.5 Update Unit tests
+# 4. API / Selenium tests
+##########################################################################################################
