699ba8313e5cb571f0ac19f4bf8d52f5a7fbb7e2
screamo1107 <screamo1107@gmail.com>
2020-06-12 08:26:40 +0200

    Update of TODO list

--- notes.py
+++ notes.py
@@ -2,9 +2,16 @@
 # TODO list:
 ##########################################################################################################
 # 1. Main logic (biopython, flask, requests) - parse sequence / rEndonuclease -> return separated sequence
-# 1.5 Unit tests (unittests / pytest)
-# 2. Store rEndonuclease list within DB (MySQL)
+# 1.5 Database (PostgreSQL/MySQL): SQL script to UPDATE DB data with rEndonuclease list
+# 2. Update main logic to suggest matching list of rEndonuclease (and separate after selection)
+# 2.5 Unit tests (unittests / pytest)
 # 3. FE part (HTML/CSS, Django, update logic to API-based, return rEndonuclease list to FE from DB)
-# 3.5 Update Unit tests
-# 4. API / Selenium tests
+# 3.5 Update FE (return suggested list, separation by selected item)
+# 4.5 Update Unit tests
+# 5. API / Selenium tests
+# ---------------------------
+# FE features:
+# - Add new rEndonuclease through FE (save to DB directly)
+# - Update visual part (separation)
+# - TBD
 ##########################################################################################################
